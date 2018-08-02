a105ddbd019bec8eb3a7c7a5d617e571566cd2d2
Peter <panleitutu@qq.com>
2018-08-03 00:04:31 +0800

    demo

---
+++ IdentityServerCenter/IdentityServerCenter/Config.cs
@@ -0,0 +1,35 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerCenter
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetResource()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api","My Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client(){
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api"}
+                }
+            };
+        }
+    }
+}
